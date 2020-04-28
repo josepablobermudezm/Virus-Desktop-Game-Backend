32a4e3706d6e2dead103684d5af1fbc69c4ca4d0
cjosue13 <44554351+cjosue13@users.noreply.github.com>
2020-04-27 21:00:47 -0600

    Update Hilo.java

--- VirusServidor/src/virusservidor/util/Hilo.java
+++ VirusServidor/src/virusservidor/util/Hilo.java
@@ -35,7 +35,7 @@ public class Hilo {
         public void run() {
             if (!finalizado) {
                 tic++;
-                if (tic == 30) {
+                if (tic == 60) {
                     timer.cancel();
                     task.cancel();
                     System.out.println("HILO TERMINADO");
